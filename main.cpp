f6642f8c42972349623865fb8c6b13fac875f6cc
suhanishende06-rgb <suhanishende06@gmail.com>
2025-12-19 21:50:43 +0530

    Add C++ addition program

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    cout << "Addition = " << a + b << endl;
+
+    return 0;
+}
